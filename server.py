468719468220b7cb82f3dc1ea1bbb134aedf73c1
rgermain <contact@germainremi.fr>
2025-11-02 00:54:32 +0100

    fix: spotdl

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-import subprocess
 import multiprocessing
+import subprocess
 import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 def download(url):
     print(f"Start download {url!r}")
-    res = subprocess.run(["spotdl", sys.argv[1], url, *sys.argv[2:]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.run(
+        ["spotdl", sys.argv[1], url, *sys.argv[2:]],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     print(f"[{'OK' if res else 'KO'}] {url!r}")
     if res.stderr:
         print(res.stderr)
@@ -19,25 +23,28 @@ class SpotdlServer(BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
             return
-            
+
         self.send_response(200)
         self.end_headers()
 
-        content_len = int(self.headers['content-length'])
+        content_len = int(self.headers["content-length"])
         data = json.loads(self.rfile.read(content_len).decode())
 
-        multiprocessing.Process(target=download, args=(data['url'],), daemon=True).start()
+        multiprocessing.Process(
+            target=download, args=(data["url"],), daemon=True
+        ).start()
+
 
 if __name__ == "__main__":
-    from spotdl.utils.arguments import parse_arguments
+    try:
+        from spotdl.utils.arguments import parse_arguments
+    except ImportError:
+        from spotdl.parsers import parse_arguments
+
     import os
 
     # auto add spotdl download arguements
-    sys.argv = [
-        sys.argv[0],
-        "download",
-        *sys.argv[1:]
-    ]
+    sys.argv = [sys.argv[0], "download", *sys.argv[1:]]
 
     # try parse spotdl arguements
     flags = parse_arguments()
